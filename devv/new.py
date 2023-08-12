ba5ff36bc31bb8b547ca3df630bc7af1d3ed3bd0
Paula Ruiz-Rodriguez <50167687+Paururo@users.noreply.github.com>
2023-08-12 10:28:10 +0200

    add ref fasta func

--- devv/new.py
+++ devv/new.py
@@ -2,3 +2,16 @@ import argparse
 from Bio import SeqIO
 from Bio.Seq import Seq
 
+def reference_fasta(fasta_file: str = 'MTB_ancestor.fas') -> str:
+    '''
+    Parse a FASTA file and return the sequence of the first record.
+
+    Parameters:
+    - fasta_file (str): Path to the FASTA file. Defaults to 'MTB_ancestor.fas'.
+
+    Returns:
+    - str: Sequence of the first record in the FASTA file.
+    '''
+    with open(fasta_file, 'r') as file:
+        fasta_sequence = next(SeqIO.parse(file, 'fasta'))
+        return str(fasta_sequence.seq)
